50aea97990e73f208487ed41648a8d4965b33810
tetarwal005 <tetarwal005@gmail.com>
2025-02-13 15:38:25 +0530

    Initial project setup with folder structure

---
+++ src/main/java/com/webknot/metro_service/MetroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.webknot.metro_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webknot/metro_service/MetroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webknot.metro_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
